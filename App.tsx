8fd4da4002aad9c3ec41dfeaa18a658085548442
sajeevah <sa.hasith@gmail.com>
2019-12-25 18:41:24 +0530

    change some sintact

--- App.tsx
+++ App.tsx
@@ -1,42 +1,10 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  // View,
-  // Text,
-  StatusBar,
-} from 'react-native';
+import React, {Component} from 'react';
+import {Text} from 'react-native';
 
-import {
-  Header,
-  // LearnMoreLinks,
-  Colors,
-  // DebugInstructions,
-  // ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-declare var global: {HermesInternal: null | {}};
-
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-});
+class App extends Component {
+  render() {
+    return <Text>Hello There</Text>;
+  }
+}
 
 export default App;
