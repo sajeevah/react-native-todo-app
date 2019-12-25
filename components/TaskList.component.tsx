6440fa113b16e2025048be99f2976f66fcd4e76e
sajeevah <sa.hasith@gmail.com>
2019-12-25 22:10:10 +0530

    styles

--- components/TaskList.component.tsx
+++ components/TaskList.component.tsx
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+  },
+});
 
 class TaskList extends Component {
   render() {
-    return <Text>Hi, this is the TaskList!</Text>;
+    return (
+      <View style={styles.container}>
+        <Text>Hi, this is the TaskList!</Text>
+      </View>
+    );
   }
 }
 
